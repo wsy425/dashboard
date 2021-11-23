e0d766a75c083ad1048da7156b109d21db7f452a
wushuyang <1565330830@qq.com>
2021-11-23 13:57:36 +0800

    初始化项目，更改默认端口

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4900';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'Dashboard',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44395',
+    redirectUri: baseUrl,
+    clientId: 'Dashboard_App',
+    responseType: 'code',
+    scope: 'offline_access Dashboard',
+    requireHttps: true
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44395',
+      rootNamespace: 'Dashboard',
+    },
+  },
+} as Environment;
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4900';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'Dashboard',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44395',
+    redirectUri: baseUrl,
+    clientId: 'Dashboard_App',
+    responseType: 'code',
+    scope: 'offline_access openid profile role email phone Dashboard',
+    requireHttps: true
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44395',
+      rootNamespace: 'Dashboard',
+    },
+  },
+} as Environment;
